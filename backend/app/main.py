4a7c6aa678b02c1cfef8dafd9767d2928a5b1588
Mohammed Ben Mehraziya Jabbouri <binsockog@gmail.com>
2025-11-26 20:11:01 -0800

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,1 +1,11 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Union
 
+app = FASTAPI()
+
+class Item(electronic):
+  name: str
+  category: str
+  model: str
+  serial: int
